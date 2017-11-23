4c29f9f9950da66e5f59916d005473d82f844d2f
Daiane Oliveira <dai.daianeaparecidaoliveira@gmail.com>
2017-11-22 22:56:05 -0200

    Ajustes client.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,28 +1,26 @@
 var problema = {};
 var xmlhttp;
 
-function httpPost(dados) {
+/**
+ * Função responsável por enviar a requisição contendo o problema para a API
+ */
+function post(dadosProblema) {
 
     xmlhttp = new XMLHttpRequest();
 
-    var link = "https://tpsimplexapi.azurewebsites.net/api/otimizacao/simplex";
-
-    xmlhttp.open("POST", link, true);
-    xmlhttp.setRequestHeader("Content-Type", "application/json");
-    xmlhttp.onreadystatechange = resultadoProblema;
-    xmlhttp.send(dados);
+    var url = "https://tpsimplexapi.azurewebsites.net/api/otimizacao/simplex";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("resposta").innerHTML =
+                this.responseText;
+        }
+    };
+    xmlhttp.send(dadosProblema);
 }
 
-function getValueInputId(idElemento) {
-    return document.getElementById(idElemento).value;
-}
-
-function resultadoProblema() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        adicionarHTML("resposta", xmlhttp.responseText);
-    }
-}
-
+//Função responsável por recuperar o valor do dropdown
 function getValueInputClass() {
     var e = document.getElementById("objetivo-selecionado");
     var strUser = e.options[e.selectedIndex].value;
@@ -30,43 +28,51 @@ function getValueInputClass() {
     return strUser;
 }
 
+//Função responsável por montar o problema para enviar para que a api resolva
 function resolver() {
     remover("problema");
     adicionar("solucao");
+
     problema = {
-        "restricoes": [{
-            "tipoRestricao": "maior",
-            "x1": getValueInputId("r1x1"),
-            "x2": getValueInputId("r1x2"),
-            "result": getValueInputId("r1Result")
-        }, {
-            "tipoRestricao": "maior",
-            "x1": getValueInputId("r2x1"),
-            "x2": getValueInputId("r2x2"),
-            "result": getValueInputId("r2Result")
-        }, {
-            "tipoRestricao": "maior",
-            "x1": getValueInputId("r3x1"),
-            "x2": getValueInputId("r3x2"),
-            "result": getValueInputId("r3Result")
-        }],
-        "funcaoObjetiva": {
-            "objetivo": getValueInputClass(),
-            "x1": getValueInputId("fox1"),
-            "x2": getValueInputId("fox2")
+            "restricoes": [{
+                    "tipoRestricao": "maior",
+                    "x1": document.getElementById("r1x1").value,
+                    "x2": document.getElementById("r1x2").value,
+                    "result": document.getElementById("r1Result").value
+                },
+                {
+                    "tipoRestricao": "maior",
+                    "x1": document.getElementById("r2x1").value,
+                    "x2": document.getElementById("r2x2").value,
+                    "result": document.getElementById("r2Result").value
+                },
+                {
+                    "tipoRestricao": "maior",
+                    "x1": document.getElementById("r3x1").value,
+                    "x2": document.getElementById("r3x2").value,
+                    "result": document.getElementById("r3Result").value
+                }
+            ],
+            "funcaoObjetiva": {
+                "objetivo": "MAX",
+                "x1": document.getElementById("fox1").value,
+                "x2": document.getElementById("fox2").value
+            }
         }
-    }
-    httpPost(JSON.stringify(problema));
+        //converte o objeto problema em json
+    post(JSON.stringify(problema));
 }
 
+//Função responsável por remover elemento através de seu id
 function remover(idElemento) {
     document.getElementById(idElemento).style.display = "none";
 }
 
+//Função responsável por adicionar elemento através de seu id
 function adicionar(idElemento) {
     document.getElementById(idElemento).style.display = "block";
 }
-
+//Função responsável por adicionar html  através de seu id
 function adicionarHTML(idElemento, conteudo) {
     document.getElementById(idElemento).innerHTML = conteudo;
 }
