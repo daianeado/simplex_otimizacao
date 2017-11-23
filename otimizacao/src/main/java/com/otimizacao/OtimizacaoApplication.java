c3d83a4f824e442bd943f8285e2be2b5e9da756f
Daiane Oliveira <dai.daianeaparecidaoliveira@gmail.com>
2017-11-22 23:33:27 -0200

    Ajustes finais API

--- otimizacao/src/main/java/com/otimizacao/OtimizacaoApplication.java
+++ otimizacao/src/main/java/com/otimizacao/OtimizacaoApplication.java
@@ -1,5 +1,7 @@
 package com.otimizacao;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,17 @@ public class OtimizacaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OtimizacaoApplication.class, args);
+		
+		loadDLL("glpk_4_63.dll");
+		loadDLL("glpk_4_63_java.dll");
+	}
+	
+	private static void loadDLL(String local) {
+		try {
+			File dll = new File(local);
+			System.load(dll.getAbsolutePath());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- otimizacao/src/main/java/com/otimizacao/rest/MixDeProducaoResource.java
+++ otimizacao/src/main/java/com/otimizacao/rest/MixDeProducaoResource.java
@@ -8,12 +8,15 @@ import com.otimizacao.service.MixDeProducaoService;
 
 import java.net.URISyntaxException;
 
+import org.gnu.glpk.GLPK;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class MixDeProducaoResource {
@@ -32,7 +35,7 @@ public class MixDeProducaoResource {
 	@GetMapping("/")
 	@Timed
 	public String getSimplex() {
-			return "Trabaho de otimização";
+			return "Trabaho de otimização:<br>GLPK UTILIZADO:" + GLPK.glp_version();
 	}
 
 }
